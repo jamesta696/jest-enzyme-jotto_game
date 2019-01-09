aa2f45683974921dba5955cd7f670330a7b28528
jamesta696 <jaysmith696@gmail.com>
2019-01-09 11:08:34 -0500

    created action type and tests

---
+++ src/actions/settingsActions.js
@@ -0,0 +1,13 @@
+export const actionTypes = {
+    CORRECT_GUESS: "CORRECT_GUESS"
+};
+
+/**
+ * @function correctGuess
+ * @returns {object} - Action object of type 'CORRECT_GUESS'
+ */
+export const correctGuess = () => {
+    return {
+        type: actionTypes.CORRECT_GUESS
+    };
+};
